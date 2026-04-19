01ce61f56f93bb390633dbbeb1ff91e49eea91aa
Oliver Zihler <oliver.zihler@codeartify.com>
2026-04-19 15:04:20 +0200

    update

--- managing-customers/src/main/kotlin/com/codeartify/managingcustomers/command/Customer.kt
+++ managing-customers/src/main/kotlin/com/codeartify/managingcustomers/command/Customer.kt
@@ -2,26 +2,37 @@ package com.codeartify.managingcustomers.command
 
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.modelling.command.AggregateCreationPolicy
 import org.axonframework.modelling.command.AggregateIdentifier
-import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.modelling.command.AggregateLifecycle.apply
+import org.axonframework.modelling.command.CreationPolicy
 import org.axonframework.spring.stereotype.Aggregate
 import java.time.LocalDate
 
 @Aggregate
-class CustomerAggregate() {
+class Customer() {
 
     @AggregateIdentifier
     lateinit var customerId: String
-
     lateinit var name: String
+    lateinit var dateOfBirth: LocalDate
+
+
 
     @CommandHandler
-    constructor(cmd: RegisterCustomerCommand) : this() {
+    @CreationPolicy(AggregateCreationPolicy.ALWAYS)
+   fun register(cmd: RegisterCustomerCommand) {
         require(cmd.customerId.isNotBlank()) { "Customer ID must not be blank" }
         require(cmd.name.isNotBlank()) { "Name must not be blank" }
         require(cmd.dateOfBirth.isBefore(LocalDate.now().minusYears(16).plusDays(1))) { "Customer must be at least 16 years old" }
 
-        AggregateLifecycle.apply(CustomerRegisteredEvent(cmd.customerId, cmd.name))
+        apply(
+            CustomerRegisteredEvent(
+                cmd.customerId,
+                cmd.name,
+                cmd.dateOfBirth
+            )
+        )
     }
 
 
@@ -29,6 +40,7 @@ class CustomerAggregate() {
     fun on(evt: CustomerRegisteredEvent) {
         customerId = evt.customerId
         name = evt.name
+        dateOfBirth = evt.dateOfBirth
     }
 
 }
