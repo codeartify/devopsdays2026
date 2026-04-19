75bf277393ffd13d01e928de6d1bd9b3411f11af
Oliver Zihler <oliver.zihler@raiffeisen.ch>
2026-04-19 15:04:20 +0200

    update

--- managing-customers/src/main/kotlin/com/codeartify/managingcustomers/command/CustomerRegisteredEvent.kt
+++ managing-customers/src/main/kotlin/com/codeartify/managingcustomers/command/CustomerRegisteredEvent.kt
@@ -1,9 +1,11 @@
 package com.codeartify.managingcustomers.command
 
 import org.axonframework.serialization.Revision
+import java.time.LocalDate
 
 @Revision("1.0")
 data class CustomerRegisteredEvent(
     val customerId: String,
-    val name: String
+    val name: String,
+    val dateOfBirth: LocalDate
 )
