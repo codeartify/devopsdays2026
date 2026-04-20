9034f54f417be2f5a4ed5b1bd31e99541485458c
Oliver Zihler <oliver.zihler@raiffeisen.ch>
2026-04-20 08:46:46 +0200

    static creation function for value object

--- managing-memberships/src/main/kotlin/com/codeartify/managingmemberships/command/MembershipId.kt
+++ managing-memberships/src/main/kotlin/com/codeartify/managingmemberships/command/MembershipId.kt
@@ -1,3 +1,14 @@
 package com.codeartify.managingmemberships.command
 
-data class MembershipId(val value: String)
+import com.fasterxml.jackson.annotation.JsonCreator
+import java.util.UUID
+
+data class MembershipId private constructor(val value: String) {
+    companion object {
+        fun generate() = MembershipId(UUID.randomUUID().toString())
+
+        @JsonCreator
+        @JvmStatic
+        fun of(value: String) = MembershipId(value)
+    }
+}
