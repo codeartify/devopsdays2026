53071a10f78f5589b6b477081c26c1bddf94dceb
Oliver Zihler <oliver.zihler@codeartify.com>
2026-04-20 08:46:46 +0200

    static creation function for value object

--- managing-memberships/src/main/kotlin/com/codeartify/managingmemberships/command/MembershipController.kt
+++ managing-memberships/src/main/kotlin/com/codeartify/managingmemberships/command/MembershipController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 @RequestMapping("/memberships")
@@ -16,26 +15,26 @@ class MembershipController(
 
     @PostMapping("/activate")
     fun activate(): ResponseEntity<String> {
-        val membershipId = MembershipId(UUID.randomUUID().toString())
+        val membershipId = MembershipId.generate()
         commandGateway.sendAndWait<String>(ActivateMembershipCommand(membershipId))
         return ResponseEntity.ok(membershipId.value)
     }
 
     @PostMapping("/{membershipId}/pause")
     fun pause(@PathVariable membershipId: String): ResponseEntity<Void> {
-        commandGateway.sendAndWait<String>(PauseMembershipCommand(MembershipId(membershipId)))
+        commandGateway.sendAndWait<String>(PauseMembershipCommand(MembershipId.of(membershipId)))
         return ResponseEntity.ok().build()
     }
 
     @PostMapping("/{membershipId}/reactivate")
     fun reactivate(@PathVariable membershipId: String): ResponseEntity<Void> {
-        commandGateway.sendAndWait<String>(ReactivateMembershipCommand(MembershipId(membershipId)))
+        commandGateway.sendAndWait<String>(ReactivateMembershipCommand(MembershipId.of(membershipId)))
         return ResponseEntity.ok().build()
     }
 
     @PostMapping("/{membershipId}/suspend")
     fun suspend(@PathVariable membershipId: String): ResponseEntity<Void> {
-        commandGateway.sendAndWait<String>(SuspendMembershipCommand(MembershipId(membershipId)))
+        commandGateway.sendAndWait<String>(SuspendMembershipCommand(MembershipId.of(membershipId)))
         return ResponseEntity.ok().build()
     }
 }
