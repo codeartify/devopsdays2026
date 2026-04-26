98a781c035554e66a69ff4af0d3a210669e91925
Oliver Zihler <oliver.zihler@raiffeisen.ch>
2026-04-26 17:38:08 +0200

    rename§

--- membership/src/main/kotlin/com/codeartify/membership/MembershipApplication.kt
+++ membership/src/main/kotlin/com/codeartify/membership/MembershipApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FitnessManagementApplication
+class MembershipApplication
 
 fun main(args: Array<String>) {
-    runApplication<FitnessManagementApplication>(*args)
+    runApplication<MembershipApplication>(*args)
 }
