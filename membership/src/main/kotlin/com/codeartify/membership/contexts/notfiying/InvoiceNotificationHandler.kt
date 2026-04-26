b983416adee7e7ba5b73dc5a1ec82ea19f7d8b22
Oliver Zihler <oliver.zihler@raiffeisen.ch>
2026-04-26 16:45:18 +0200

    move stuff around

--- membership/src/main/kotlin/com/codeartify/membership/contexts/notfiying/InvoiceNotificationHandler.kt
+++ membership/src/main/kotlin/com/codeartify/membership/contexts/notfiying/InvoiceNotificationHandler.kt
@@ -3,6 +3,7 @@ package com.codeartify.membership.contexts.notfiying
 import com.codeartify.membership.contexts.billing.InvoiceIssuedEvent
 import com.codeartify.membership.contexts.customer_cache.CustomerCacheRepository
 import org.axonframework.config.ProcessingGroup
+import org.axonframework.eventhandling.DisallowReplay
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.stereotype.Component
 
@@ -14,6 +15,7 @@ class InvoiceNotificationHandler(
 ) {
 
     @EventHandler
+    // @DisallowReplay // use this if replay should not resend
     fun on(event: InvoiceIssuedEvent) {
         val customer = customerRepository.findById(event.customerId.value)
             .orElseThrow { IllegalStateException("Customer not found") }
