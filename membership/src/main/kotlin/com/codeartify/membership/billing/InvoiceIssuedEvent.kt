86127b3430dcf3853afbcb74d717d206ed8a404a
Oliver Zihler <oliver.zihler@codeartify.com>
2026-04-26 21:40:24 +0200

    upgrade axon, java, kotlin

--- membership/src/main/kotlin/com/codeartify/membership/billing/InvoiceIssuedEvent.kt
+++ membership/src/main/kotlin/com/codeartify/membership/billing/InvoiceIssuedEvent.kt
@@ -2,8 +2,10 @@ package com.codeartify.membership.billing
 
 import com.codeartify.membership.managing_memberships.domain.CustomerId
 import com.codeartify.membership.managing_memberships.domain.MembershipId
+import org.axonframework.messaging.eventhandling.annotation.Event
 import java.time.LocalDate
 
+@Event(version = "1.0")
 data class InvoiceIssuedEvent(
     val invoiceId: String,
     val membershipId: MembershipId,
--- membership/src/main/kotlin/com/codeartify/membership/managing_plans/domain/events/PlanDefinedEvent.kt
+++ membership/src/main/kotlin/com/codeartify/membership/managing_plans/domain/events/PlanDefinedEvent.kt
@@ -1,8 +1,9 @@
 package com.codeartify.membership.managing_plans.domain.events
 
 import com.codeartify.membership.managing_plans.domain.values.PlanId
+import org.axonframework.messaging.eventhandling.annotation.Event
 
-
+@Event(version = "1.0")
 data class PlanDefinedEvent(
     val planId: PlanId,
     val title: String,
