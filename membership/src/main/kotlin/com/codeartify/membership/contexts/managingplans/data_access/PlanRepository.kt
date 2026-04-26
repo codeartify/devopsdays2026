5be8bb05d385df90c3cf7d539f9bc3bc5524c5df
Oliver Zihler <oliver.zihler@codeartify.com>
2026-04-26 16:16:11 +0200

    remove dead code

--- membership/src/main/kotlin/com/codeartify/membership/contexts/managingplans/data_access/PlanRepository.kt
+++ membership/src/main/kotlin/com/codeartify/membership/contexts/managingplans/data_access/PlanRepository.kt
@@ -5,7 +5,6 @@ import com.codeartify.membership.contexts.managingplans.domain.values.PlanDurati
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PlanRepository : JpaRepository<Plan, String> {
-    fun existsByDuration(duration: PlanDuration): Boolean
     fun existsByDurationAndIdNot(duration: PlanDuration, id: String): Boolean
     fun findAllByOrderByDurationAsc(): List<Plan>
 }
