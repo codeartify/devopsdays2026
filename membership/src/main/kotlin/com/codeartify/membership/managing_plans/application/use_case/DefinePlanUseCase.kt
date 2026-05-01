d51559de35d5b04e5b9c965a6a3db9a8f2244d46
Oliver Zihler <oliver.zihler@codeartify.com>
2026-05-01 14:59:50 +0200

    Updates

--- membership/src/main/kotlin/com/codeartify/membership/managing_plans/application/use_case/DefinePlanUseCase.kt
+++ membership/src/main/kotlin/com/codeartify/membership/managing_plans/application/use_case/DefinePlanUseCase.kt
@@ -3,12 +3,19 @@ package com.codeartify.membership.managing_plans.application.use_case
 import com.codeartify.membership.managing_plans.application.request.CreatePlanRequest
 import com.codeartify.membership.managing_plans.data_access.PlanRepository
 import com.codeartify.membership.managing_plans.domain.Plan
-import com.codeartify.membership.managing_plans.domain.values.*
+import com.codeartify.membership.managing_plans.domain.events.PlanDefinedEvent
+import com.codeartify.membership.managing_plans.domain.values.PlanDescription
+import com.codeartify.membership.managing_plans.domain.values.PlanDuration
+import com.codeartify.membership.managing_plans.domain.values.PlanId
+import com.codeartify.membership.managing_plans.domain.values.PlanPrice
+import com.codeartify.membership.managing_plans.domain.values.PlanTitle
+import org.axonframework.messaging.eventhandling.gateway.EventGateway
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
 class DefinePlanUseCase(
+    private val eventGateway: EventGateway,
     private val planRepository: PlanRepository
 ) {
     @Transactional
@@ -17,6 +24,19 @@ class DefinePlanUseCase(
         val plan = toPlan(request, planId)
 
         planRepository.save(plan)
+
+        eventGateway.publish(
+            listOf(
+                PlanDefinedEvent(
+                    plan.planId,
+                    plan.title.value,
+                    plan.description.value,
+                    plan.price.value,
+                    plan.duration.value
+                )
+            )
+        )
+
         return planId
     }
 
