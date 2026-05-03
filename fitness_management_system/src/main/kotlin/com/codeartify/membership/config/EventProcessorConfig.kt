879a1e5416835b180a2429b033ecba9a6c609798
Oliver Zihler <oliver.zihler@codeartify.com>
2026-05-03 13:39:27 +0200

    Updates

---
+++ fitness_management_system/src/main/kotlin/com/codeartify/membership/config/EventProcessorConfig.kt
@@ -0,0 +1,36 @@
+package com.codeartify.membership.config
+
+import org.axonframework.extension.spring.config.EventProcessorDefinition
+import org.axonframework.messaging.eventhandling.processing.streaming.pooled.PooledStreamingEventProcessorConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class EventProcessorConfig {
+
+    @Bean
+    fun billingProcessorDefinition(): EventProcessorDefinition =
+        EventProcessorDefinition.pooledStreaming("membership-invoice-policy")
+            .assigningHandlers { descriptor ->
+                descriptor.beanType()?.packageName == "com.codeartify.membership.billing"
+            }
+
+    @Bean
+    fun membershipProjectionProcessorDefinition(): EventProcessorDefinition =
+        EventProcessorDefinition.pooledStreaming("membership-projection")
+            .assigningHandlers { descriptor ->
+                descriptor.beanType()?.name == "com.codeartify.membership.managing_memberships.use_case.query_memberships.MembershipProjection"
+            }
+    @Bean
+    fun notifyingCustomersProjectionProcessorDefinition(): EventProcessorDefinition =
+        EventProcessorDefinition.pooledStreaming("notifying-customers")
+            .assigningHandlers { descriptor ->
+                descriptor.beanType()?.packageName == "com.codeartify.membership.notifying_customers"
+            }
+            .customized(::startAtLatestToken)
+
+    private fun startAtLatestToken(
+        config: PooledStreamingEventProcessorConfiguration
+    ): PooledStreamingEventProcessorConfiguration =
+        config.initialToken { tokenSource -> tokenSource.latestToken(null) }
+}
